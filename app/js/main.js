b6b551872a9e38cfd517f53c62dc02f5c3bf9d83
ThunderInTheGrass <propeller8924@gmail.com>
2022-09-04 17:54:04 +0800

    search

--- app/js/main.js
+++ app/js/main.js
@@ -11,7 +11,7 @@
 
 function mobileMenu() {
   let menuBtn = document.querySelector('.nav-mobile__btn');
-  let mobileMenuInner = document.querySelector('.nav-mobile')
+  let mobileMenuInner = document.querySelector('.nav-mobile');
 
   menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('mobile-btn--active');
@@ -19,3 +19,17 @@ function mobileMenu() {
   })
 }
 mobileMenu();
+
+function headerSearch() {
+  let searchBtn = document.querySelector('.search__btn');
+  let searchBtnOff = document.querySelector('.search__btn_off');
+  let searchBtnMobile = document.querySelector('.search__btn_mobile');
+  let search = document.querySelector('.search');
+  let mobileMenuBtn = document.querySelector('.nav-mobile__btn-wrapper');
+
+  searchBtnMobile.addEventListener('click', () => {
+    search.classList.add('d-block');
+    searchBtnMobile.classList.add('d-none');
+  })
+}
+headerSearch();
