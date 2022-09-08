d3b6792aed4a3e6abb28eee0bb0f233a1a4b67b5
ThunderInTheGrass <propeller8924@gmail.com>
2022-09-08 22:05:29 +0800

    ---------

--- app/js/main.js
+++ app/js/main.js
@@ -62,7 +62,7 @@ function popupForm() {
   const btns = document.querySelectorAll(".modal-btn");
   const modalOverlay = document.querySelector(".modal-overlay ");
   const modals = document.querySelectorAll(".modal");
-  const modalCloseBtn = document.querySelector('.modal__close-btn');
+  const modalCloseBtn = document.querySelector(".modal__close-btn");
 
   btns.forEach((el) => {
     el.addEventListener("click", (e) => {
@@ -91,3 +91,45 @@ function popupForm() {
   });
 }
 popupForm();
+
+function scrollAnimation() {
+  const animItems = document.querySelectorAll("._anim-items");
+
+  if (animItems.length > 0) {
+    window.addEventListener("scroll", animOnScroll);
+    function animOnScroll() {
+      for (let k = 0; k < animItems.length; k++) {
+        const animItem = animItems[k];
+        const animItemHeight = animItem.offsetHeight;
+        const animItemOffset = offset(animItem).top;
+        const animStart = 4;
+
+        let animItemPoint = window.innerHeight - animItemHeight / animStart;
+        if (animItemHeight > window.innerHeight) {
+          animItemPoint = window.innerHeight - window.innerHeight / animStart;
+        }
+
+        if (
+          pageYOffset > animItemOffset - animItemPoint &&
+          pageYOffset < animItemOffset + animItemHeight
+        ) {
+          animItem.classList.add("_active");
+        } else {
+          animItem.classList.remove("_active");
+        }
+      }
+    }
+    function offset(el) {
+      const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+    }
+
+    setTimeout(() => {
+      animOnScroll();
+    }, 100);
+    animOnScroll();
+  }
+}
+scrollAnimation();
